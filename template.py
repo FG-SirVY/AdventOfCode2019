29b75d541649a042e6d1b0fb0872476a4949cc2c
Henrik Zimmermann <henrik@zi-data.com>
2022-01-04 23:54:43 +0100

    Initial commit.

---
+++ template.py
@@ -0,0 +1,18 @@
+###############################################################################
+# Day x, Task y                                                               #
+###############################################################################
+
+import aoc_util
+
+
+year = 2020
+day = 0
+data_str = """"""
+
+
+def task(data_set: list[str]) -> int:
+    pass
+
+
+aoc_util.run_with_data_str(task, data_str)
+aoc_util.run_with_data_set(task, year, day)
